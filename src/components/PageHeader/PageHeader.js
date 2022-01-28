d8b0d5cf094436b23185a4cbee6b1ac0e5f92c4a
JayJay1024 <lygm1062@gmail.com>
2022-01-28 14:07:44 +0800

    fix: i18n

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -9,6 +9,7 @@ import logo_light from '../../logo-light.svg'
 import menu_icon from './img/menu.png'
 import close_icon from './img/close.png'
 import {withTranslation} from "react-i18next";
+import i18n from '../../locales/i18n';
 
 // const CustomToggle = React.forwardRef(({children, onClick}, ref) => (
 //     <div
@@ -88,7 +89,7 @@ class PageHeader extends Component {
 
 
     render() {
-        const {theme, t, i18n} = this.props
+        const {theme, t} = this.props
         const {visible} = this.state
         let curLang = i18n.language;
         return (
@@ -106,7 +107,7 @@ class PageHeader extends Component {
                                     <Nav.Link href="/#product" onClick={this.onClickAnchor}><span
                                         className={styles.NavLink}>{t('header:product')}</span></Nav.Link>
                                     <Nav.Link href="/jobs"><span className={styles.NavLink}>{t('header:jobs')}</span></Nav.Link>
-                                    {curLang.toLowerCase && curLang.toLowerCase() === 'zh-cn' ? <div className={styles.Language}
+                                    {curLang.toLowerCase() === 'zh-cn' ? <div className={styles.Language}
                                                                               onClick={() => this.changeLng('en-us')}>English</div> :
                                         <div className={styles.Language}
                                              onClick={() => this.changeLng('zh-cn')}>中文</div>}
