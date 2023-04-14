ed3baa5bc0451ae9acde9a6ed0e556a3a772ba3e
JayJay1024 <lygm1062@gmail.com>
2023-04-14 16:14:40 +0800

    fix: failed to build

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,20 @@
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-const path = require('path');
+// const PrerenderSPAPlugin = require('prerender-spa-plugin');
+// const path = require('path');
 
 module.exports = (config, env) => {
-  if (env === 'production') {
-    config.plugins = config.plugins.concat([
-      new PrerenderSPAPlugin({
-        routes: [
-            '/', 
-            '/about',
-            '/news',
-            '/404'
-        ],
-        staticDir: path.join(__dirname, 'build'),
-      }),
-    ]);
-  }
+  // if (env === 'production') {
+  //   config.plugins = config.plugins.concat([
+  //     new PrerenderSPAPlugin({
+  //       routes: [
+  //           '/', 
+  //           '/about',
+  //           '/news',
+  //           '/404'
+  //       ],
+  //       staticDir: path.join(__dirname, 'build'),
+  //     }),
+  //   ]);
+  // }
 
   return config;
 };
