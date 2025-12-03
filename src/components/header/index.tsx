057ccd2290f176a17c50a9e00de9c17fa1c1c722
bigbigpeng <bigbigpeng3@gmail.com>
2025-12-03 16:41:48 +0800

    Fix build (#66)
    * remove portfolio page & related code
    
    * Fix github action build

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,24 +2,9 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import MobileNav from './mobile-nav';
 
 export default function Header() {
-  const NavLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
-    const isActive = usePathname() === href;
-
-    console.log('isActive', isActive, usePathname(), href);
-
-    return (
-      <Link
-        href={href}
-        className={`relative hidden text-[16px] font-normal uppercase leading-6 text-white after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:origin-bottom-right after:scale-x-0 after:bg-white after:transition-transform after:duration-300 after:ease-out hover:after:origin-bottom-left hover:after:scale-x-100 md:block ${isActive ? 'after:scale-x-100' : ''} tracking-[1px]`}
-      >
-        {children}
-      </Link>
-    );
-  };
   return (
     <header className="container h-[50px] md:h-[100px]">
       <nav className="flex h-full w-full items-center justify-between gap-10">
